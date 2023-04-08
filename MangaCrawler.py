b4d40dad9958ade3373c7fe9ba45dbcd7ec9bba4
jia-xiaodong <jiaxdg@gmail.com>
2023-04-08 22:56:20 +0800

    +batch downloads

--- MangaCrawler.py
+++ MangaCrawler.py
@@ -1,14 +1,11 @@
-#!usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
+from tkinter import ttk, filedialog, messagebox
 import threading
 import queue
-from urllib.request import urlopen
-from urllib.request import Request
+from urllib.request import urlopen, Request
 from urllib.parse import quote, quote_plus, urlparse
 from hashlib import md5
 import os
@@ -39,6 +36,14 @@ def url_quote(url):
     if len(quoted) > 5 and len(quoted[5]) > 0:
         result = '{}#{}'.format(result, quoted[5])  # fragment
     return result
+
+
+def batch_jobs(template: str, from_: int, to_: int, pattern: str):
+    for i in range(from_, to_ + 1):
+        order = '{order:{width}}'.format(order=i, width=pattern).strip()
+        entity = template.replace('(*)', order)
+        yield entity, i
+    yield None, None
 
 
 class WebPageHandler(HTMLParser):
@@ -132,33 +137,40 @@ class DownloadStatus(Enum):
 
 
 class ThreadDownloader(threading.Thread):
-    def __init__(self, url, dst, timeout=3, callback=None):
+    def __init__(self, url, dst, timeout=3, retry=1, callback=None):
         threading.Thread.__init__(self)
         self._url = url  # source URL
         self._dst = dst  # destination folder
         self._timeout = timeout
+        self._retry = retry
         self._cb = callback
         self._status = DownloadStatus.Unknown
 
     def run(self):
-        try:
-            user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50'}
-            req = Request(self._url, headers=user_agent, unverifiable=True)
-            ifo = urlopen(req, timeout=self._timeout)
-            content = ifo.read()
-            ifo.close()
-            self._size = len(content)
-            if not self.file_exists(content):
-                with open(self._dst, 'wb') as ofo:
-                    ofo.write(content)
-            self._status = DownloadStatus.Successful
-            if self._cb is not None:
-                self._cb(self)
-        except Exception as e:
-            print('%s: %s' % (self._url, e))
+        user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50'}
+        req = Request(self._url, headers=user_agent, unverifiable=True)
+        content = None
+        for i in range(0, self._retry):
+            try:
+                ifo = urlopen(req, timeout=self._timeout)
+                content = ifo.read()
+                ifo.close()
+                break
+            except Exception as e:
+                print('%s: %s' % (self._url, e))
+        # 失败
+        if content is None:
             self._status = DownloadStatus.Failed
             if self._cb is not None:
                 self._cb(self)
+            return
+        # 成功
+        if not self.file_exists(content):
+            with open(self._dst, 'wb') as ofo:
+                ofo.write(content)
+        self._status = DownloadStatus.Successful
+        if self._cb is not None:
+            self._cb(self)
 
     def file_exists(self, content):
         """
@@ -213,6 +225,18 @@ class MainWnd(tk.Frame):
         #
         frame = tk.Frame(self)
         frame.pack(side=tk.TOP, expand=tk.NO, fill=tk.BOTH)
+        tk.Label(frame, text='Wild Card Pattern:').pack(side=tk.LEFT)
+        self._wildcard_pattern = tk.StringVar(value='04')
+        tk.Entry(frame, textvariable=self._wildcard_pattern, width=4).pack(side=tk.LEFT, expand=tk.NO)
+        tk.Label(frame, text='Range:').pack(side=tk.LEFT)
+        self._wildcard_from = tk.IntVar(value=1)
+        tk.Spinbox(frame, textvariable=self._wildcard_from, from_=1, to=999, width=3).pack(side=tk.LEFT)
+        tk.Label(frame, text='-->').pack(side=tk.LEFT)
+        self._wildcard_to = tk.IntVar(value=99)
+        tk.Spinbox(frame, textvariable=self._wildcard_to, from_=1, to=999, width=3).pack(side=tk.LEFT)
+        #
+        frame = tk.Frame(self)
+        frame.pack(side=tk.TOP, expand=tk.NO, fill=tk.BOTH)
         tk.Label(frame, text='Hanlder:').pack(side=tk.LEFT)
         self._handlers = dict()
         for handler in [HachiRawHandler(), ParallelParadiseOnlineHandler()]:
@@ -257,7 +281,10 @@ class MainWnd(tk.Frame):
         tk.Label(frame, text='Timeout:').pack(side=tk.LEFT)
         self._timeout = tk.IntVar()
         tk.Spinbox(frame, textvariable=self._timeout, from_=3, to=9, width=2).pack(side=tk.LEFT)
-        self._btn = tk.Button(frame, text='Download Images', command=self.onclick_download_segments)
+        tk.Label(frame, text='Retry:').pack(side=tk.LEFT)
+        self._retry = tk.IntVar()
+        tk.Spinbox(frame, textvariable=self._retry, from_=1, to=5, width=2).pack(side=tk.LEFT)
+        self._btn = tk.Button(frame, text='Download Images', command=self.onclick_download_images)
         self._btn.pack(side=tk.LEFT)
         #
         self._auto = tk.BooleanVar(value=True)
@@ -266,21 +293,42 @@ class MainWnd(tk.Frame):
         self._job_queue = queue.Queue()
         self._downloaders = []
         self._running = False
+        self._url_generator = None
+        self._dir_generator = None
+        self._save_dir = None
 
     def onclick_analyze_url(self):
         url = self._url.get().strip()
         dst = self._tmp_dir.get().strip()
         if len(url) == 0 or len(dst) == 0:
             return
+        wildcard_url = '(*)' in url
+        wildcard_dst = '(*)' in dst
+        if any([wildcard_url, wildcard_dst]) and not all([wildcard_url, wildcard_dst]):
+            messagebox.askquestion(MainWnd.WND_TITLE, 'wild card pattern not match')
+            return
+        if wildcard_url:
+            self._url_generator = batch_jobs(url, self._wildcard_from.get(), self._wildcard_to.get(), self._wildcard_pattern.get())
+            url, current = next(self._url_generator)
+            if url is None:
+                return
+        if wildcard_dst:
+            self._dir_generator = batch_jobs(dst, self._wildcard_from.get(), self._wildcard_to.get(), self._wildcard_pattern.get())
+            dst, current = next(self._dir_generator)
+            if dst is None:
+                return
+            else:
+                self._wildcard_from.set(current)
         if not os.path.exists(dst):
             os.mkdir(dst)
         url = url_quote(url)
-        job = ThreadDownloader(url, os.path.join(dst, MainWnd.WEB_PAGE), self._timeout.get(), self.on_web_page_downloaded)
+        self._save_dir = dst
+        job = ThreadDownloader(url, os.path.join(self._save_dir, MainWnd.WEB_PAGE), self._timeout.get(), self._retry.get(), self.on_web_page_downloaded)
         job.start()
 
     def on_web_page_downloaded(self, job: ThreadDownloader):
         if not job.is_successful():
-            messagebox.showerror(MainWnd.WND_TITLE, 'Failed to download web page')
+            messagebox.showerror(MainWnd.WND_TITLE, 'Failed to download web page:\n%s' % job._url)
             return
         parse_result = urlparse(job._url)
         if parse_result.netloc not in self._handlers:
@@ -290,14 +338,15 @@ class MainWnd(tk.Frame):
 
     def load_local_web_page(self):
         dst = self._tmp_dir.get().strip()
-        if len(dst) == 0:
+        if len(dst) == 0 or '(*)' in dst:
             return
-        web_page = self.web_page()
+        web_page = os.path.join(dst.strip(), MainWnd.WEB_PAGE)
         if not os.path.exists(web_page):
             return
         active_handler = self._active_handler.get()
         if len(active_handler) == 0:
             return
+        self._save_dir = dst.strip()
         self.load_links(web_page, active_handler)
 
     def load_links(self, file, active_handler):
@@ -307,7 +356,7 @@ class MainWnd(tk.Frame):
             iid = 'I%04d' % i
             self._links.insert('', tk.END, iid=iid, values=(i, url, ''))
         if self._auto.get():
-            self.onclick_download_segments()
+            self.enqueue_all_jobs()
 
     def paste_from_clipboard(self):
         url = self.clipboard_get().strip()
@@ -345,11 +394,7 @@ class MainWnd(tk.Frame):
         self.clipboard_clear()
         self.clipboard_append(url)
 
-    def onclick_download_segments(self):
-        urls = self._links.get_children()
-        if len(urls) == 0:
-            return
-        #
+    def onclick_download_images(self):
         if self._running:
             self._running = False   # global signal to stop running jobs
             self._btn.config(text='Download')
@@ -358,22 +403,25 @@ class MainWnd(tk.Frame):
             self._downloaders[:] = []
             return
         #
+        self.enqueue_all_jobs()
+
+    def enqueue_all_jobs(self):
+        urls = self._links.get_children()
+        if len(urls) == 0:
+            return
+        #
         self._running = True
         self._btn.config(text='Cancel')
-        try:
-            self.clear_queue()
-            for i in urls:
-                self._job_queue.put(i)
-                self._links.set(i, column='state', value='')
-            #
-            self._progress.set(0)
-            self._progressbar.config(maximum=self._job_queue.qsize())
-            #
-            self.after(100, self.update_progress)
-        except Exception as e:
-            print(e)
-            self._btn.config(text='Download')
-            self._running = False
+        #
+        self.clear_queue()
+        for i in urls:
+            self._job_queue.put(i)
+            self._links.set(i, column='state', value='')
+        #
+        self._progress.set(0)
+        self._progressbar.config(maximum=self._job_queue.qsize())
+        #
+        self.after(100, self.update_progress)
 
     def clear_queue(self):
         self._downloaders[:] = []
@@ -413,8 +461,8 @@ class MainWnd(tk.Frame):
             sn, url, state = self._links.item(iid, 'values')
             _, ext = url_image_name(url)
             url = url_quote(url)
-            dst = os.path.join(self._tmp_dir.get(), 'img_%04d.%s' % (int(sn), ext))
-            job = ThreadDownloader(url, dst, self._timeout.get())
+            dst = os.path.join(self._save_dir, 'img_%04d.%s' % (int(sn), ext))
+            job = ThreadDownloader(url, dst, self._timeout.get(), self._retry.get())
             job.iid = iid  # attach a temporary attribute
             self._downloaders.append(job)
             job.start()
@@ -422,22 +470,47 @@ class MainWnd(tk.Frame):
         #
         if len(self._downloaders) > 0:
             self.after(100, self.update_progress)
-        else:
+            return
+        # 任务全部下载完，无需保留网页
+        if len(self._links.get_children()) == 0:
+            os.remove(os.path.join(self._save_dir, MainWnd.WEB_PAGE))
+        # 如果不继续，提醒
+        if not self.can_automate_next():
             self.after(100, self.notify_finish)
+            return
+        # 尝试下一链接
+        url, current = next(self._url_generator)
+        dst, current = next(self._dir_generator)
+        if url is None or dst is None:
+            self._url_generator = None
+            self._dir_generator = None
+            self.after(100, self.notify_finish)
+            return
+        # 正式下载下一链接
+        if not os.path.exists(dst):
+            os.mkdir(dst)
+        self._wildcard_from.set(current)
+        url = url_quote(url)
+        self._save_dir = dst
+        job = ThreadDownloader(url, os.path.join(dst, MainWnd.WEB_PAGE), self._timeout.get(), self._retry.get(), self.on_web_page_downloaded)
+        job.start()
 
     def notify_finish(self):
         self._running = False
         self._btn.config(text='Download')
-        if len(self._links.get_children()) == 0:
-            os.remove(self.web_page())
         messagebox.showinfo(MainWnd.WND_TITLE, 'All segments are downloaded.')
-
-    def web_page(self):
-        dst = self._tmp_dir.get().strip()
-        return os.path.join(dst, MainWnd.WEB_PAGE)
 
     def on_choose_handler(self, _):
         pass
+
+    def can_automate_next(self):
+        if self._auto.get() is False:
+            return False
+        if len(self._links.get_children()) > 0:
+            return False
+        if self._url_generator is None or self._dir_generator is None:
+            return False
+        return True
 
 
 def main():
